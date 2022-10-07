8d78fc8fd18cdf1016627bd5428e8477272c2bfe
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-07 18:03:16 -0400

    added required const to server

---
+++ server.js
@@ -0,0 +1,18 @@
+///linking dependecies///
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const express = require('express');
+
+const sequelize = require("./config/connection");
+
+const routes = require('./controllers');
+const sequelize = require('./connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
