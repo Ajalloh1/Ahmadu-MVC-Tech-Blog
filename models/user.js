0f8d5c3a176072b89031bc746fa3763d1e79f7ec
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-10 06:54:02 -0400

    added user in userjs

--- models/user.js
+++ models/user.js
@@ -6,3 +6,58 @@ class User extends Model {
   checkPassword(loginPW) {
     return bcrypt.compareSync(loginPW, this.password);
   }
+}
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [6],
+        },
+      },
+    },
+    
+    {
+      hooks: {
+        beforeCreate: async (newUserData) => {
+          console.log(newUserData.username);
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        beforeBulkUpdate: async (newUserData) => {
+          updateUserData.attributes.password = await bcrypt.hash(
+            newUserData.attributes.password,
+            10
+          );
+          return newUserData;
+        },
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user',
+    }
+  );
+  
+  module.exports = User;
