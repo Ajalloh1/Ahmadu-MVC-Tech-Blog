6908e1e648b4b37e619cd2209a4ae0f8a3f9dc3a
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-11 10:55:24 -0400

    added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -50,4 +50,34 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  //added more option here//
   
+///delete//
+
+router.delete("/:id", withAuth, (req, res) => {
+    User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+
+
+
+
+
+  module.exports = router;
