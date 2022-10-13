cb0589ad5fe13223271fe782448683d1341421de
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-12 20:24:51 -0400

    added in commentjs

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,23 +1,23 @@
-async function newFormHandler(event) {
+async function newpost(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
     const post_text = document.querySelector('textarea[name="post-text"]').value;
   
     const response = await fetch(`/api/blogs/`, {
-        method: 'POST',
-        body: JSON.stringify({ blog_title, blog_text, user_id }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-    
-      if (response.ok) {
-        document.location.replace(`/dashboard`);
-      } else {
-        alert('Failed to submit reply.');
-      }
-    };
+      method: 'POST',
+      body: JSON.stringify({ blog_title, blog_text, user_id }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace(`/dashboard`);
+    } else {
+      alert('Failed to submit reply.');
+    }
+  };
   
   document
     .querySelector(".new-post-form")
-    .addEventListener("submit", newFormHandler);
+    .addEventListener("submit", newpost);
   
